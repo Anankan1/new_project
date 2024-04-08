41d760857152677059fe2845b3d7fdc5dc50dd32
Nisat <129756080+nisathav@users.noreply.github.com>
2024-04-08 08:21:11 +0530

    angle return

--- face_detector_angle.py
+++ face_detector_angle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 #this code returns the angle of the face movement
+from time import sleep
 import rospy
 import os
 import cv2
@@ -18,12 +19,15 @@ class FaceDetector:
         self.img_sub = rospy.Subscriber('/rgbd_camera/rgb/image_raw', Image, self.image_callback, queue_size=10)
         self.depth_sub = rospy.Subscriber('/rgbd_camera/depth/image_raw', Image, self.depth_callback, queue_size=10)
         #self.face_pub = rospy.Publisher('/face_position', Point, queue_size=10)
+        
         self.face_angle_pub = rospy.Publisher('/face_angle_position', Point, queue_size=10)
-        self.bridge_object = CvBridge()
 
-        # self.face_center = Point()
+        self.bridge_object = CvBridge()
         # self.face_center.x = 0
         # self.face_center.y = 0
+        self.list1=[0]
+        self.list2=[0]
+        self.list3=[0]
 
     def depth_callback(self, msg):
         try:
@@ -33,8 +37,6 @@ class FaceDetector:
 
     def image_callback(self, msg):
         try:
-            list1=[0]
-            list2=[0]
             
             # face_center.x=0
             # face_center.y=0
@@ -43,14 +45,15 @@ class FaceDetector:
             faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
             print(f"faces: {faces}")
             result_radians = Point()
-            list3=[0]
 
-            if ( list1[-1]== 0) and (list2[-1] == 0):
-                angle_z=list3[-1]-0.1
-                list3.append(angle_z)
-                result_radians.z =list3[-1]
+            if ( self.list1[-1]== 0) and (self.list2[-1] == 0):
+                angle_1=self.list3[-1]-0.001
+                angle_z=round(angle_1,3)
+                self.list3.append(angle_z)
+                result_radians.z = self.list3[-1]
+                #sleep(1)
             else:
-                result_radians.z=list3[-1]
+                result_radians.z = self.list3[-1]
 
             print(f"z: {result_radians.z}")
             if self.depth_image is not None:
@@ -58,15 +61,16 @@ class FaceDetector:
                     face_center= Point()
                     face_center.x = x + w / 2
                     face_center.y = y + h / 2
-                    list1.append(face_center.x)
-                    list2.append(face_center.y)
-                    #calculate the depth value
-                    depth_value = self.depth_image[int(self.face_center.y), int(self.face_center.x)]
-                    self.face_center.z = depth_value  # Assign the depth value
+                    self.list1.append(face_center.x)
+                    self.list2.append(face_center.y)
+                    depth_value = self.depth_image[int(face_center.y), int(face_center.x)]
+                    face_center.z = depth_value  # Assign the depth value
 
                     ppi=0.03076/118
                     x_difference=face_center.x -400
-                    y_difference=300- face_center.y
+                    # y_difference=300- face_center.y
+                    y_difference=face_center.y -300
+
                     x_axis=ppi*x_difference
                     y_axis=ppi*y_difference
                     z_axis=face_center.z
@@ -75,13 +79,14 @@ class FaceDetector:
                     result_radians.y = math.atan(y_axis / z_axis)
                     # Convert radians to degrees if needed
                     # result_x_degrees = math.degrees(result_x_radians)
-                    # result_y_degrees = math.degrees(result_y_radians)
+                    # result_y_degrees = math.degrees (result_y_radians)
 
                     #self.face_pub.publish(self.face_center)
                     cv2.rectangle(cv_image, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
             print(f"angles: {result_radians}")
             self.face_angle_pub.publish(result_radians)
+
             cv2.imshow('Face Detection', cv_image)
             cv2.waitKey(1)
 
